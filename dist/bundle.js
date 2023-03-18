302ea155aac3860d2bce5d355bd425a75a731c2e
Konstantin Samartsev <samarcev737@yandex.ru>
2023-03-18 12:10:50 +0500

    card.js add new version class method

--- dist/bundle.js
+++ dist/bundle.js
@@ -87,42 +87,131 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (/* binding */ cards)
 /* harmony export */ });
 function cards() {
-   //запрос к серверу createCard(data)
-   const getResource = async (url) => {
-      const res = await fetch(url);
-      if(!res.ok) {
-         throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+      class Menu {
+      constructor () {
+         this.getData();
+         this.transfer = 74;
       }
-      return await res.json();
+      changeToUSD(price) {
+         return Math.floor(price / this.transfer);
+      }
+      buildItemMenu (img, altimg, title, descr, price) {
+         price = this.changeToUSD(price);
+         const menuConteiner = document.querySelector('.container_menu');
+         const menuItem = 
+            `<div class="menu__item">
+               <img src=${img} alt=${altimg}>
+               <h3 class="menu__item-subtitle">${title}</h3>
+               <div class="menu__item-descr">${descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${price}</span> $/день</div>
+               </div>
+            </div>`
+         menuConteiner.insertAdjacentHTML('beforeend', menuItem);
+      }
+      getData () {
+         const getResource = async (url) => {
+            const res = await fetch(url);
+            if(!res.ok) {
+               throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+            }
+            return await res.json();
+         }
+         getResource('http://localhost:3000/menu')
+         .then(data => data.forEach(({img, altimg, title, descr, price}) => {
+                  this.buildItemMenu(img, altimg, title, descr, price);
+               }
+            )
+         );
+      }
    }
-
-   getResource('http://localhost:3000/menu')
-      .then(data => createCard(data));
-
-
-   //Имплементация панелей меню
-   function createCard(data) { //беру массив объектов с сервера(в db.json)
-      data.forEach(({img, altimg, title, descr, price}) => { 
-         const changeToUSD = Math.floor(price / 74.5);
-         const menuConteiner = document.querySelector('.container_menu')
-         const element = document.createElement('div');
-         element.classList.add('menu__item');
-         element.innerHTML = `
-         <div class="menu__item">
-            <img src=${img} alt=${altimg}>
-            <h3 class="menu__item-subtitle">Меню ${title}</h3>
-            <div class="menu__item-descr">${descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-               <div class="menu__item-cost">Цена:</div>
-               <div class="menu__item-total"><span>${changeToUSD}</span> $/день</div>
-            </div>
-         </div>
-         `;
-         menuConteiner.append(element);
-      });
-   };
+   new Menu();
 }
+
+
+   // //запрос к серверу createCard(data)
+   // const getResource = async (url) => {
+   //    const res = await fetch(url);
+   //    if(!res.ok) {
+   //       throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+   //    }
+   //    return await res.json();
+   // }
+
+   // getResource('http://localhost:3000/menu')
+   //    .then(data => createCard(data));
+
+
+   // //Имплементация панелей меню
+   // function createCard(data) { //беру массив объектов с сервера(в db.json)
+   //    data.forEach(({img, altimg, title, descr, price}) => {
+   //       const changeToUSD = Math.floor(price / 74.5);
+   //       const menuConteiner = document.querySelector('.container_menu')
+   //       const element = document.createElement('div');
+   //       element.classList.add('menu__item');
+   //       element.innerHTML = `
+   //       <div class="menu__item">
+   //          <img src=${img} alt=${altimg}>
+   //          <h3 class="menu__item-subtitle">Меню ${title}</h3>
+   //          <div class="menu__item-descr">${descr}</div>
+   //          <div class="menu__item-divider"></div>
+   //          <div class="menu__item-price">
+   //             <div class="menu__item-cost">Цена:</div>
+   //             <div class="menu__item-total"><span>${changeToUSD}</span> $/день</div>
+   //          </div>
+   //       </div>
+   //       `;
+   //       menuConteiner.append(element);
+   //    });
+   // };
+
+
+   // const getResource = async (url) => {
+   //    const res = await fetch(url);
+   //    if(!res.ok) {
+   //       throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+   //    }
+   //    return await res.json();
+   // }
+
+   // getResource('http://localhost:3000/menu')
+   //    .then(data => data.forEach(({img, altimg, title, descr, price}) => {
+   //                   new ItemMenu(img, altimg, title, descr, price);
+   //                }
+   //             )
+   //          );
+   // class ItemMenu {
+   //    constructor (img, altimg, title, descr, price) {
+   //       this.img = img;
+   //       this.altimg = altimg;
+   //       this.title = title;
+   //       this.price = price;
+   //       this.descr = descr;
+   //       this.transfer = 74;
+   //       this.changeToUSD();//работает сразу внутри
+   //    }
+   //    changeToUSD() {
+   //       this.price = Math.floor(this.price / this.transfer) //перевод в доллары
+   //    }
+   //    buildItemMenu () {
+   //       const menuConteiner = document.querySelector('.container_menu')
+   //       const menuItem = 
+   //          `<div class="menu__item">
+   //             <img src=${this.img} alt=${this.altimg}>
+   //             <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
+   //             <div class="menu__item-descr">${this.descr}</div>
+   //             <div class="menu__item-divider"></div>
+   //             <div class="menu__item-price">
+   //                <div class="menu__item-cost">Цена:</div>
+   //                <div class="menu__item-total"><span>${this.price}</span> $/день</div>
+   //             </div>
+   //          </div>`
+   //       menuConteiner.insertAdjacentHTML('beforeend', menuItem);
+   //    }
+   // }
+
 
 /***/ }),
 
@@ -267,13 +356,6 @@ function modal() {
          closeModalWindow();
       }, 3000);
    };
-
-   //получим доступ к нашей базе данных db.json через fetch
-   fetch('http://localhost:3000/menu')
-      .then(data => data.json())
-      .then(data => console.log(data));
-
-
 }
 
 /***/ }),
--- src/cards.js
+++ src/cards.js
@@ -1,4 +1,48 @@
 export default function cards() {
+      class Menu {
+      constructor () {
+         this.getData();
+         this.transfer = 74;
+      }
+      changeToUSD(price) {
+         return Math.floor(price / this.transfer);
+      }
+      buildItemMenu (img, altimg, title, descr, price) {
+         price = this.changeToUSD(price);
+         const menuConteiner = document.querySelector('.container_menu');
+         const menuItem = 
+            `<div class="menu__item">
+               <img src=${img} alt=${altimg}>
+               <h3 class="menu__item-subtitle">${title}</h3>
+               <div class="menu__item-descr">${descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${price}</span> $/день</div>
+               </div>
+            </div>`
+         menuConteiner.insertAdjacentHTML('beforeend', menuItem);
+      }
+      getData () {
+         const getResource = async (url) => {
+            const res = await fetch(url);
+            if(!res.ok) {
+               throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+            }
+            return await res.json();
+         }
+         getResource('http://localhost:3000/menu')
+         .then(data => data.forEach(({img, altimg, title, descr, price}) => {
+                  this.buildItemMenu(img, altimg, title, descr, price);
+               }
+            )
+         );
+      }
+   }
+   new Menu();
+}
+
+
    // //запрос к серверу createCard(data)
    // const getResource = async (url) => {
    //    const res = await fetch(url);
@@ -14,7 +58,7 @@ export default function cards() {
 
    // //Имплементация панелей меню
    // function createCard(data) { //беру массив объектов с сервера(в db.json)
-   //    data.forEach(({img, altimg, title, descr, price}) => { 
+   //    data.forEach(({img, altimg, title, descr, price}) => {
    //       const changeToUSD = Math.floor(price / 74.5);
    //       const menuConteiner = document.querySelector('.container_menu')
    //       const element = document.createElement('div');
@@ -79,57 +123,3 @@ export default function cards() {
    //       menuConteiner.insertAdjacentHTML('beforeend', menuItem);
    //    }
    // }
-
-   // const getResource = async (url) => {
-   //    const res = await fetch(url);
-   //    if(!res.ok) {
-   //       throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-   //    }
-   //    return await res.json();
-   // }
-
-   // getResource('http://localhost:3000/menu')
-   //    .then(data => data.forEach(({img, altimg, title, descr, price}) => {
-   //                   new ItemMenu(img, altimg, title, descr, price);
-   //                }
-   //             )
-   //          );
-   class Menu {
-      constructor () {
-         let data = this.getData();
-         this.transfer = 74;
-         this.changeToUSD();//работает сразу внутри
-      }
-      changeToUSD() {
-         this.price = Math.floor(this.price / this.transfer) //перевод в доллары
-      }
-      buildItemMenu (item) {
-         console.log(item);
-         const menuConteiner = document.querySelector('.container_menu')
-         const menuItem = 
-            `<div class="menu__item">
-               <img src=${img} alt=${altimg}>
-               <h3 class="menu__item-subtitle">Меню ${title}</h3>
-               <div class="menu__item-descr">${descr}</div>
-               <div class="menu__item-divider"></div>
-               <div class="menu__item-price">
-                  <div class="menu__item-cost">Цена:</div>
-                  <div class="menu__item-total"><span>${price}</span> $/день</div>
-               </div>
-            </div>`
-         menuConteiner.insertAdjacentHTML('beforeend', menuItem);
-      }
-      getData () {
-         const getResource = async (url) => {
-            const res = await fetch(url);
-            if(!res.ok) {
-               throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-            }
-            return await res.json();
-         }
-         getResource('http://localhost:3000/menu')
-            .then(data => data.forEach(console.log(data))
-         );
-      }
-   }
-}
